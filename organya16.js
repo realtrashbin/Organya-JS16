12e7422857fa3e34967d143510ac322c995f7a22
Bi_N <122274598+realtrashbin@users.noreply.github.com>
2026-03-15 16:06:28 -0400

    Update organya16.js

--- organya16.js
+++ organya16.js
@@ -275,12 +275,12 @@
         if (window.Organya) return;
 
         console.log("Initializing Organya...");
-        const res = await fetch("WAVE100.bin");
+        const res = await fetch("wavetable.bin");
         const buf = await res.arrayBuffer();
         const view = new DataView(buf);
         waveTable = new Int8Array(buf);
         // Thanks CaveStory.org!
-        const res_d = await fetch("DrumWaves.bin"); //'_d' for 'drum'. Beyond that, code is unchanged
+        const res_d = await fetch("wavetable.bin"); //'_d' for 'drum'. Beyond that, code is unchanged
         const buf_d = await res_d.arrayBuffer();
         const view_d = new DataView(buf_d);
         drumWaveTable = new Int8Array(buf_d);
