a1e5eadf3e7c5233578d09b61ccadde512ac6b01
Bi_N <122274598+realtrashbin@users.noreply.github.com>
2026-03-26 18:57:07 -0400

    Update organya16.js

--- organya16.js
+++ organya16.js
@@ -17,9 +17,8 @@
             }
 
             const orgVersion = view.getUint16(p, true); p += 2;
-            if (orgVersion != 0x3631) {
-                throw "File is not org16";
-                return;
+            if (orgVersion != 0x3631) { //16
+                throw "Invalid version.";
             }
 
             this.wait = view.getUint16(p, true); p += 2;
@@ -82,6 +81,7 @@
          */
         constructor(data) {
             this.song = new Song(data);
+            this.MeasxStep=this.song.meas[0]*this.song.meas[1];
             this.node = null;
             this.onUpdate = null;
             this.t = 0;
@@ -89,7 +89,9 @@
             this.samplesPerTick = 0;
             this.samplesThisTick = 0;
             this.state = [];
-            for (let i = 0; i < 32; i++) {
+            this.mutedTracks=[];
+            this.selectedTrack=0;
+            for (let i = 0; i < 16; i++) {
                 this.state[i] = {
                     t: 0,
                     key: 0,
@@ -97,7 +99,6 @@
                     octave: 0,
                     pan: 0.0,
                     vol: 1.0,
-                    vol_log: 1.0,
                     length: 0,
                     num_loops: 0,
                     playing: false,
@@ -118,44 +119,43 @@
                 rightBuffer[sample] = 0;
 
                 for (let i = 0; i < 32; i++) {
-                    const trackState = this.state[i];
-                    if (trackState.playing) {
-                        const samples = (i < 16) ? 256 : drums[i - 16].samples;
+                    if (this.state[i].playing) {
+                        const samples = (i < 16) ? 256 : drums[this.song.instruments[i].wave].samples;
 
-                        trackState.t += (trackState.frequency / this.sampleRate) * advTable[trackState.octave];
+                        this.state[i].t += (this.state[i].frequency / this.sampleRate) * advTable[this.state[i].octave];
 
-                        if ((trackState.t | 0) >= samples) {
-                            if (trackState.looping && trackState.num_loops != 1) {
-                                trackState.t %= samples;
-                                if (trackState.num_loops != 1)
-                                    trackState.num_loops -= 1;
+                        if ((this.state[i].t | 0) >= samples) {
+                            if (this.state[i].looping && this.state[i].num_loops != 1) {
+                                this.state[i].t %= samples;
+                                if (this.state[i].num_loops != 1)
+                                    this.state[i].num_loops -= 1;
 
                             } else {
-                                trackState.t = 0;
-                                trackState.playing = false;
+                                this.state[i].t = 0;
+                                this.state[i].playing = false;
                                 continue;
                             }
                         }
 
-                        const t = trackState.t & ~(advTable[trackState.octave] - 1);
+                        const t = this.state[i].t & ~(advTable[this.state[i].octave] - 1);
                         let pos = t % samples;
                         let pos2 = !this.looping && t == samples ?
                             pos
-                            : ((trackState.t + advTable[trackState.octave]) & ~(advTable[trackState.octave] - 1)) % samples;
+                            : ((this.state[i].t + advTable[this.state[i].octave]) & ~(advTable[this.state[i].octave] - 1)) % samples;
                         const s1 = i < 16
                             ? (waveTable[256 * this.song.instruments[i].wave + pos] / 256)
-                            : (((waveTable[drums[i - 16].filePos + pos] & 0xff) - 0x80) / 256);
+                            : (((drumWaveTable[drums[this.song.instruments[i].wave].filePos + pos] & 0xff) - 0x80) / 256);
                         const s2 = i < 16
                             ? (waveTable[256 * this.song.instruments[i].wave + pos2] / 256)
-                            : (((waveTable[drums[i - 16].filePos + pos2] & 0xff) - 0x80) / 256);
-                        const fract = (trackState.t - pos) / advTable[trackState.octave];
+                            : (((drumWaveTable[drums[this.song.instruments[i].wave].filePos + pos2] & 0xff) - 0x80) / 256);
+                        const fract = (this.state[i].t - pos) / advTable[this.state[i].octave];
 
                         // perform linear interpolation
                         let s = s1 + (s2 - s1) * fract;
 
-                        s *= trackState.vol_log;
+                        s *= Math.pow(10, ((this.state[i].vol - 255) * 8) / 2000);
 
-                        const pan = (panTable[trackState.pan] - 256) * 10;
+                        const pan = (panTable[this.state[i].pan] - 256) * 10;
                         let left = 1, right = 1;
 
                         if (pan < 0) {
@@ -171,146 +171,198 @@
 
                 if (++this.samplesThisTick == this.samplesPerTick) {
                     this.playPos += 1;
+                    this.updateTimeDisplay();
                     this.samplesThisTick = 0;
 
                     if (this.playPos == this.song.end) {
                         this.playPos = this.song.start;
+                        this.updateTimeDisplay();
                     }
                 }
             }
         }
-
+        
+        homeOrg() {
+            this.playPos = 0;
+            this.updateTimeDisplay();
+        }
+        
+        backMeas() {
+            if (this.playPos-(this.MeasxStep+this.playPos%this.MeasxStep)>=0){
+                this.playPos-=(this.MeasxStep+this.playPos%this.MeasxStep);
+            }
+            this.updateTimeDisplay();
+        }
+        
+        nextMeas() {
+            this.playPos+=(this.MeasxStep-this.playPos%this.MeasxStep); //to go to beginning of next measure
+            this.updateTimeDisplay();
+        }
+        
+        
+        updateTimeDisplay() {
+            currentMeasDisplay.innerHTML=this.playPos/(this.MeasxStep) | 0;
+            currentStepDisplay.innerHTML=this.playPos%(this.MeasxStep);
+            this.update(); //this line is so as to update the display when next/previous is pressed, even when not playing
+        }
+        
         update() {
             if (this.onUpdate) this.onUpdate(this);
+            
+            this.whichMuted();
 
             for (let track = 0; track < 16; track++) {
-                const note = this.song.tracks[track].find((n) => n.pos == this.playPos); // TODO: this feels inefficient
-                const trackState = this.state[track];
+                if (!(this.mutedTracks.includes(track))) {
+                const note = this.song.tracks[track].find((n) => n.pos == this.playPos);
                 if (note) {
                     if (note.key != 255) {
                         const octave = ((note.key / 12) | 0);
                         const key = note.key % 12;
 
-                        if (trackState.key == 255) {
-                            trackState.key = note.key;
+                        if (this.state[track].key == 255) {
+                            this.state[track].key = note.key;
 
-                            trackState.frequency = freqTable[key] * octTable[octave] + (this.song.instruments[track].freq - 1000);
-                            if (this.song.instruments[track].pipi != 0 && !trackState.playing) {
-                                trackState.num_loops = ((octave + 1) * 4);
+                            this.state[track].frequency = freqTable[key] * octTable[octave] + (this.song.instruments[track].freq - 1000);
+                            if (this.song.instruments[track].pipi != 0 && !this.state[track].playing) {
+                                this.state[track].num_loops = ((octave + 1) * 4);
                             }
-                        } else if (trackState.key != note.key) {
-                            trackState.key = note.key;
-                            trackState.frequency = freqTable[key] * octTable[octave] + (this.song.instruments[track].freq - 1000);
+                        } else if (this.state[track].key != note.key) {
+                            this.state[track].key = note.key;
+                            this.state[track].frequency = freqTable[key] * octTable[octave] + (this.song.instruments[track].freq - 1000);
                         }
 
-                        if (this.song.instruments[track].pipi != 0 && !trackState.playing) {
-                            trackState.num_loops = ((octave + 1) * 4);
+                        if (this.song.instruments[track].pipi != 0 && !this.state[track].playing) {
+                            this.state[track].num_loops = ((octave + 1) * 4);
                         }
 
-                        trackState.octave = octave;
-                        trackState.playing = true;
-                        trackState.looping = true;
-                        trackState.length = note.len;
+                        this.state[track].octave = octave;
+                        this.state[track].playing = true;
+                        this.state[track].looping = true;
+                        this.state[track].length = note.len*2;
+                        //I have no clue why, but at some point along the way (I think during adding support for new drums) the notes were getting cut off half way. Multiplying length by 2 is a sloppy fix that just works. Please help ;_;
                     }
 
-                    if (trackState.key != 255) {
-                        if (note.vol != 255) {
-                            trackState.vol = note.vol;
-                            trackState.vol_log = Math.pow(10, ((note.vol - 255) * 8) / 2000);
-                        }
-                        if (note.pan != 255) trackState.pan = note.pan;
+                    if (this.state[track].key != 255) {
+                        if (note.vol != 255) this.state[track].vol = note.vol;
+                        if (note.pan != 255) this.state[track].pan = note.pan;
                     }
+                } //ending the 'skip muted tracks' if-block here, rather than at the end, because otherwise, muting while a note played would make that note get stuck
                 }
-
-                if (trackState.length == 0) {
-                    if (trackState.key != 255) {
+                if (this.state[track].length == 0) {
+                    if (this.state[track].key != 255) {
                         if (this.song.instruments[track].pipi == 0)
-                            trackState.looping = false;
+                            this.state[track].looping = false;
 
-                        trackState.playing = false;
-                        trackState.key = 255;
+                        this.state[track].playing = false;
+                        this.state[track].key = 255;
                     }
                 } else {
-                    trackState.length--;
+                    this.state[track].length--;
                 }
+            
             }
 
             for (let track = 16; track < 32; track++) {
+                if (!(this.mutedTracks.includes(track))) {
                 const note = this.song.tracks[track].find((n) => n.pos == this.playPos);
-                const trackState = this.state[track];
                 if (!note) continue;
 
                 if (note.key != 255) {
-                    trackState.frequency = note.key * 800 + 100;
-                    trackState.t = 0;
-                    trackState.playing = true;
+                    this.state[track].frequency = note.key * 800 + 100;
+                    this.state[track].t = 0;
+                    this.state[track].playing = true;
                 }
 
-                if (note.vol != 255) {
-                    trackState.vol = note.vol;
-                    trackState.vol_log = Math.pow(10, ((note.vol - 255) * 8) / 2000);
-                }
-                if (note.pan != 255) trackState.pan = note.pan;
+                if (note.vol != 255) this.state[track].vol = note.vol;
+                if (note.pan != 255) this.state[track].pan = note.pan;
             }
+        }
         }
 
         stop() {
             this.node.disconnect();
             this.ctx.close();
         }
+        
+        pause() {
+            this.node.disconnect();
+            this.ctx.close();
+        }
 
-        play() {
+        play(argument) {
             this.ctx = new (window.AudioContext || window.webkitAudioContext)();
             this.sampleRate = this.ctx.sampleRate;
             this.samplesPerTick = (this.sampleRate / 1000) * this.song.wait | 0;
             this.samplesThisTick = 0;
 
             this.node = this.ctx.createScriptProcessor(8192, 0, 2);
-            this.node.onaudioprocess = (e) => this.synth(e.outputBuffer.getChannelData(0), e.outputBuffer.getChannelData(1));
-            this.node.connect(this.ctx.destination);
+            
+            if(argument=='doPlay'){ //the point of this bit is to change the display as soon as a new org is selected
+                this.node.onaudioprocess = (e) => this.synth(e.outputBuffer.getChannelData(0), e.outputBuffer.getChannelData(1));
+                this.node.connect(this.ctx.destination);
+            }
+        }
+        
+        whichMuted() {
+            var checkedValues = [];
+            var inputElements = document.getElementsByClassName('mute');
+            for(var i=0; inputElements[i]; ++i){
+                if(inputElements[i].checked){
+                    checkedValues.push(Number(inputElements[i].value));
+                }
+            }
+            this.mutedTracks=checkedValues;
         }
     }
 
- window.initOrganya = async () => {
+    window.initOrganya = async () => {
         if (window.Organya) return;
-
+        
+        //splitting waves and drums into separate wavetables
+        
         console.log("Initializing Organya...");
-        const res = await fetch("newwaves.bin");
+        const res = await fetch("WAVE100.bin");
         const buf = await res.arrayBuffer();
         const view = new DataView(buf);
         waveTable = new Int8Array(buf);
+        
+        const res_d = await fetch("DrumWaves.bin"); //'_d' for 'drum'. Beyond that, code is unchanged
+        const buf_d = await res_d.arrayBuffer();
+        const view_d = new DataView(buf_d);
+        drumWaveTable = new Int8Array(buf_d);
 
-        for (let i = 256 * 100; i < waveTable.length - 4; i++) {
-            if (view.getUint32(i, true) == 0x45564157) {
+        for (let i = 0; i < drumWaveTable.length - 4; i++) {
+            if (view_d.getUint32(i, true) == 0x45564157) { //"WAVE"
                 i += 4;
-                const riffId = view.getUint32(i, true); i += 4;
-                const riffLen = view.getUint32(i, true); i += 4;
-                if (riffId != 0x20746d66) {
+                const riffId = view_d.getUint32(i, true); i += 4;
+                const riffLen = view_d.getUint32(i, true); i += 4;
+                if (riffId != 0x20746d66) { //"fmt "
                     console.error("Invalid RIFF chunk ID");
                     continue;
                 }
 
                 const startPos = i;
-                const aFormat = view.getUint16(i, true); i += 2;
+                const aFormat = view_d.getUint16(i, true); i += 2;
                 if (aFormat != 1) {
                     console.error("Invalid audio format");
                     i = startPos + riffLen;
                     continue;
                 }
 
-                const channels = view.getUint16(i, true); i += 2;
+                const channels = view_d.getUint16(i, true); i += 2;
                 if (channels != 1) {
                     console.error("Only 1 channel files are supported");
                     i = startPos + riffLen;
                     continue;
                 }
 
-                const samples = view.getUint32(i, true); i += 10; // skip rate + padding
-                const bits = view.getUint16(i, true); i += 2;
-                const wavData = view.getUint32(i, true); i += 4;
-                const wavLen = view.getUint32(i, true); i += 4;
+                const samples = view_d.getUint32(i, true); i += 10; // skip rate + padding
+                const bits = view_d.getUint16(i, true); i += 2;
+                i+=14; //dunno why but there's 14 extraneous bytes in the new drum files at this point
+                const wavData = view_d.getUint32(i, true); i += 4;
+                const wavLen = view_d.getUint32(i, true); i += 4;
 
-                if (wavData != 0x61746164) {
+                if (wavData != 0x61746164) { //"data"
                     i = startPos + riffLen;
                     continue;
                 }
@@ -323,5 +375,3 @@
         window.Organya = Organya;
     };
 })();
-
-
